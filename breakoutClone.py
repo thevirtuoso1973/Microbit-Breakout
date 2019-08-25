d7f588c3ed0380313c562ac546f545dd48fc51ee
thevirtuoso1973 <ct@localhost.localdomain>
2019-08-25 20:52:54 +0100

    add main code loop

--- breakoutClone.py
+++ breakoutClone.py
@@ -14,6 +14,35 @@ board = [
         [9, 9, 9, 9, 9]
         [9, 9, 9, 9, 9]
         [0, 0, 0, 0, 0]
-        [0, 9, 0, 0, 0]
+        [0, 1, 0, 0, 0]
         [9, 9, 0, 0, 0]
         ]
+ballPos = [3, 1]
+playerPos = 0 # along the bottom row
+
+while not gameWon() and not gameLost():
+    for i in range(4):
+        updatePlayer()
+        sleep(250)
+    updatePlayer()
+    updateBoard()
+
+def gameWon():
+    hasWon = True
+    for i in range(2):
+        for j in range(5):
+            if board[i][j] == 9:
+                hasWon = False
+    return hasWon
+
+def gameLost():
+    #TODO
+    pass
+
+def updatePlayer():
+    #TODO
+    pass
+
+def updateBoard():
+    #TODO
+    pass
